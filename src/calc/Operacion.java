18206c6a9be18d59d6f5c105ecc95e4d575daa12
octaviopezoa <octavio.pezoa@gmail.com>
2018-07-11 20:29:24 -0400

    Termino clase padre Operacion

--- src/calc/Operacion.java
+++ src/calc/Operacion.java
@@ -10,5 +10,20 @@ package calc;
  * @author tardis
  */
 public class Operacion {
+    double n1;
+    double n2;
+    double res;
+    char operacion;
+
+    public Operacion(double n1, double n2, char operacion) {
+        this.n1 = n1;
+        this.n2 = n2;
+        this.operacion = operacion;
+    }
+    
+    public void mostrarResultado(){
+        System.out.println(this.n1+" "+this.n2+" = "+.this.res);
+    }
+    
     
 }
